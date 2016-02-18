f5260bdbef419d710234c4c9087d0f172f629036
Allon Lei <alan001lhl@sina.com>
2016-02-18 12:03:58 +0800

    Try to fix os version problem. Add install_lham_agent.sh to compile lham_agent at install time. Add api for_agent_install.php for install_lham_agent.sh to random generate source download path. add function in basic.php to validate host by hostname. 2016-02-18, Alan Lei.

--- php/basic/basic.php
+++ php/basic/basic.php
@@ -28,7 +28,21 @@ function validateHost($ip_input) {
                 } else return "ERROR: host_query_error";
         } else return "ERROR: ldap_service_error";
 
-} // validateUser() complete.
+} // validateHost() complete. 'will be aborted'
+
+function validateHostByHostname($hostname_input) {
+        $ldap_conn = ldap_connect(LDAP_HOST,LDAP_PORT);
+        if(ldap_bind($ldap_conn,LDAP_BINDDN,LDAP_BINDPW)) {
+                if ($result_object = ldap_search($ldap_conn,HOST_BASE_DN,"(ipNetworkNumber=$hostname_input)",array('cn'))) {
+                        $info = ldap_get_entries($ldap_conn,$result_object);
+                        if ($info['count'] !== 0 ) {
+                                //echo $info[0]['uid'][0] . "\n";
+                                return "host_valid";
+                        } else return "ERROR: host_invalid"; 
+                } else return "ERROR: host_query_error";
+        } else return "ERROR: ldap_service_error";
+
+} // validateHostbyHostname() complete.
 
 function queryHost($host_ip,$host_attr) {
         $ldap_conn = ldap_connect(LDAP_HOST,LDAP_PORT);
@@ -49,5 +63,23 @@ function queryHost($host_ip,$host_attr) {
         } else echo "ERROR: ldap_service_error";
 }
 
+function queryHostByHostname($hostname,$host_attr) {
+        $ldap_conn = ldap_connect(LDAP_HOST,LDAP_PORT);
+        if(ldap_bind($ldap_conn,LDAP_BINDDN,LDAP_BINDPW)) {
+                if ($result_object = ldap_search($ldap_conn,HOST_BASE_DN,"(cn=$hostname)",array($host_attr))) {
+                        $info = ldap_get_entries($ldap_conn,$result_object);
+                        if ($info['count'] == 0 ) {
+				            echo "ERROR: host_invalid";
+                        } else if($info['count'] == 1) {
+				            if (($user_count = $info[0][$host_attr]["count"]) >= 1){
+					            for($i=0; $i<$user_count; $i++) {
+						            $user_dn_split = preg_split('/[=,]+/',$info[0][$host_attr][$i]);
+						            echo $user_dn_split[1] . "\n";
+					            }
+				            } else echo "ERROR: bad_host_entry";
+			            } else echo "ERROR: hostname_not_uniq_in_ldap"; 
+                } else echo"ERROR: host_query_error";
+        } else echo "ERROR: ldap_service_error";
+}
 
 ?>
